c37698d62ca91c9f9762dade78338c525a82d3ac
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-08-30 14:39:47 +0300

    Кажется, работает в обоих браузерах, но периодически падают тесты

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as Oc
+from selenium.webdriver.firefox.options import Options as Of
 
 
 def pytest_addoption(parser):
@@ -19,13 +19,13 @@ def browser(request):
 
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options = Oc()
+        options.set_preference('intl.accept_languages', user_language)
         browser = webdriver.Chrome(options=options)
 
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        options = Options()
+        options = Of()
         options.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=options)
     else:
