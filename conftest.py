9d45b26eef2cf6069f7346e47827919a352a72c8
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-08-30 11:49:54 +0300

    определение языка для каждого браузера

--- conftest.py
+++ conftest.py
@@ -15,20 +15,18 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-
-    fp = webdriver.FirefoxProfile()
-    fp.set_preference("intl.accept_languages", user_language)
-    browser = webdriver.Firefox(firefox_profile=fp)
 
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
