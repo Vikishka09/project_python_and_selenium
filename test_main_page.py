afe8d3962220e649f0fbc8113c2c830b5f07ce76
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-01 10:54:20 +0300

    Добавили переход на страницу логина и проверки, что есть форма входа, форма регистрации и что url браузера содержит слово 'login'

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,5 @@
-from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
