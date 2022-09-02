ff8af476bda55fa1a8a16b5d01f189c9d519d16e
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-02 13:37:17 +0300

    Добавили локаторы для ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,14 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+
+    BOOK_NAME = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+
+    BOOK_PRICE = (By.CSS_SELECTOR, '#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color')
+
+    BASKET_SUM = (By.CSS_SELECTOR,
+                  '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
