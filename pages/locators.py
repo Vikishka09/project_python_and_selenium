40c21cfedbb6072d1af0beaa61c0a9b467d1eaeb
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-07 14:44:07 +0300

    Добавила локатор алерта об успешном добавлении товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -22,3 +22,5 @@ class ProductPageLocators:
 
     BASKET_SUM = (By.CSS_SELECTOR,
                   '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
+
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
