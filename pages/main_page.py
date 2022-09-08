e3ffe651295e047119a11f7c07f317194582d543
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-08 16:43:22 +0300

    Методы перенесли в BasePage, тут теперь заглушка

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,11 @@
 from pages.base_page import BasePage
-from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+    def __init__(self, *args, **kwargs):
+        super.__init__(*args, **kwargs)
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
+#метод __init__ вызывается при создании объекта.
+# Конструктор выше с ключевым словом super на самом деле только вызывает конструктор класса предка и передает ему все те аргументы,
+# которые мы передали в конструктор MainPage
