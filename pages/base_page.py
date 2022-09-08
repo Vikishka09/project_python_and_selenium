236337552650c8370dea89ea1ecddcd1e8655b11
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-08 16:42:38 +0300

    Перенесли методы перехода на страницу логина и проверки ссылки для перехода на страницу логина из MainPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,8 +3,10 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+from pages.locators import BasePageLocators
 
 
+#нужно расположить методы по алфавиту
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -49,3 +51,12 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+
