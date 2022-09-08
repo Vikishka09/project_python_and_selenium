3e3c96d3484df3e1a4dd1b15b3bb9c14c80d8793
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-08 16:41:41 +0300

    Добавился класс BasePageLocators и локаторы внутри него

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators:
