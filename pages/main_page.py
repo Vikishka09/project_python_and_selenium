f53e9fe2bc469127c7d4beb9675e6f001cf9b4f9
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-09 13:33:44 +0300

    Поменяла инит, из-за которого не работали тесты (не хватало селфа)

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,9 +2,8 @@ from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super.__init__(*args, **kwargs)
-
+    def __init__(self, browser, url, *args, **kwargs):
+        super().__init__(browser, url)
 
 #метод __init__ вызывается при создании объекта.
 # Конструктор выше с ключевым словом super на самом деле только вызывает конструктор класса предка и передает ему все те аргументы,
