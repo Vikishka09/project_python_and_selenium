a5674d96f5619a4913e31acc6678280a753ed77b
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-09 13:37:30 +0300

    Файл с тестами для страницы корзины. Первый тест проверяет, что корзина пустая при переходе в неё с главной страницы. Второй тест аналогично при переходе в корзину со страницы продукта

---
+++ test_basket_page.py
@@ -0,0 +1,23 @@
+from pages.basket_page import BasketPage
+from pages.main_page import MainPage
+from pages.product_page import ProductPage
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/expert-c-programming_179/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
