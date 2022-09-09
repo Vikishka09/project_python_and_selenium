589e812a2872076f1a41258ba010e691001a7ddc
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-09 13:32:08 +0300

    Добавила локатор корзины

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR,
+                   "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
 
 
 class MainPageLocators:
@@ -13,20 +15,19 @@ class MainPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
 class ProductPageLocators:
     ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
-
     BOOK_NAME_ON_ALERT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
-
     BOOK_NAME = (By.CSS_SELECTOR, '#content_inner > article > div.row > div.col-sm-6.product_main > h1')
-
     BOOK_PRICE = (By.CSS_SELECTOR, '#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color')
-
     BASKET_SUM = (By.CSS_SELECTOR,
                   '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
-
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
+
+
+class BasketPageLocators:
+    PRODUCTS_ON_BASKET = (By.CSS_SELECTOR, '#content_inner > div.basket-title.hidden-xs > div > h2')
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, '#content_inner > p')
