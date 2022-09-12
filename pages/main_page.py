ba08858d73e87751831e33391973768e23079213
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-12 13:26:17 +0300

    Добавила аргументы в инит

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,7 @@ from pages.base_page import BasePage
 
 class MainPage(BasePage):
     def __init__(self, browser, url, *args, **kwargs):
-        super().__init__(browser, url)
+        super().__init__(browser, url, *args, **kwargs)
 
 #метод __init__ вызывается при создании объекта.
 # Конструктор выше с ключевым словом super на самом деле только вызывает конструктор класса предка и передает ему все те аргументы,
