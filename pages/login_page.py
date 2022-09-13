9bef2acb996d3d24d933622d6990fdb84ba5c8e2
DESKTOP-236VDHR\Виктория <victoriakishkina@gmail.com>
2022-09-13 12:37:36 +0300

    Добавила метод для регистрации нового пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,7 @@ from pages.locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -19,3 +20,10 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REPEAT_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
